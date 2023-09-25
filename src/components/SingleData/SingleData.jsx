7219c948f6f69185577a8aed0cf0bee0e80417cb
Alien Brain <murad.dub@gmail.com>
2023-09-25 10:37:25 +0600

    dynamic color code added

--- src/components/SingleData/SingleData.jsx
+++ src/components/SingleData/SingleData.jsx
@@ -1,9 +1,16 @@
 import { Link } from "react-router-dom";
 const SingleData = ({ singleData }) => {
   // console.log(singleData);
-  const { image, title, category } = singleData || {};
+  const { image, title, category, colors } = singleData || {};
+  console.log(colors.color);
+  // const color = "blue";
+  // const colorVariants = {
+  //   blue: "bg-blue-600 hover:bg-blue-500",
+  //   red: "bg-red-600 hover:bg-red-500",
+  // };
+
   return (
-    <div className="p-4">
+    <div className={`${colors.color}`}>
       <Link to={"/allData/:id"}>
         <img src={image} alt="Alternative Image" />
         <button>{category}</button>
