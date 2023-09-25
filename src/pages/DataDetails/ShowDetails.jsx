ece2f8146d32de231d97f223e3b370da934a9160
Alien Brain <murad.dub@gmail.com>
2023-09-25 20:27:35 +0600

    Donation data stored in localstorage done

--- src/pages/DataDetails/ShowDetails.jsx
+++ src/pages/DataDetails/ShowDetails.jsx
@@ -1,11 +1,21 @@
+import { saveDonationData } from "../../utility/localstorage";
+
 const ShowDetails = ({ data }) => {
   console.log(data);
-  const { image, price, title, description } = data;
+  const { id, image, price, title, description } = data;
+
+  const handleDonationList = () => {
+    saveDonationData(id);
+  };
+
   return (
     <div>
       <img className="w-full" src={image} alt="Alternative Image" />
       <div className="bg-[#0B0B0B80] opacity-50 p-6 -mt-16 h-16 z-10">
-        <button className="bg-red-600 text-white opacity-100 z-20">
+        <button
+          onClick={handleDonationList}
+          className="bg-red-600 text-white opacity-100 z-20"
+        >
           Donate ${price}
         </button>
       </div>
