760bc6a92779566e76f338ae6baebd84b725a2a3
Alien Brain <murad.dub@gmail.com>
2023-09-26 22:22:43 +0600

    Search items filtering and render done

--- src/components/AllData/AllData.jsx
+++ src/components/AllData/AllData.jsx
@@ -1,10 +1,22 @@
 import { useLoaderData } from "react-router-dom";
 import SingleData from "../SingleData/SingleData";
+import { useState } from "react";
 
 const AllData = ({ fromSearchInput }) => {
-  const allData = useLoaderData();
+  let allData = useLoaderData();
   // console.log(allData);
   console.log(fromSearchInput);
+  // const [displayData, setDisplayData] = useState(allData);
+  if (
+    fromSearchInput === "Education" ||
+    fromSearchInput === "Health" ||
+    fromSearchInput === "Clothing" ||
+    fromSearchInput === "Food"
+  ) {
+    allData = allData.filter((aData) => aData.category === fromSearchInput);
+    // setDisplayData(temp);
+  }
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 py-6">
       {allData.map((singleData) => (
